f17fa8d6c7b77108e12aee05c19f9338c665899b
Eric Foster <ericf@zipcoders-MacBook-Pro-3.local>
2018-08-02 18:16:10 -0400

    playlist api and controller update

--- src/main/java/com/geeknight/GeekNightApp/api/PlaylistRestController.java
+++ src/main/java/com/geeknight/GeekNightApp/api/PlaylistRestController.java
@@ -3,10 +3,12 @@ package com.geeknight.GeekNightApp.api;
 import com.geeknight.GeekNightApp.repositories.PlaylistRepo;
 import com.geeknight.GeekNightApp.repositories.entities.Playlist;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.geeknight.GeekNightApp.config.PlaylistApis.GET_PLAYLIST_BY_TITLE;
+import static com.geeknight.GeekNightApp.config.PlaylistApis.PLAYLIST_BASE_URL;
 
 @RestController
 @RequestMapping("api/v1/")
@@ -15,17 +17,17 @@ public class PlaylistRestController {
     @Autowired
     private PlaylistRepo playlistRepo;
 
-    @RequestMapping(value = "playlist/{title}", method = RequestMethod.GET)
+    @GetMapping(path = GET_PLAYLIST_BY_TITLE)
     public Playlist getPlaylistByTitle(@PathVariable String title){
         return playlistRepo.getPlaylistByTitle(title);
     }
 
-    @RequestMapping(value = "playlist", method = RequestMethod.GET)
+    @GetMapping(path = PLAYLIST_BASE_URL)
     public List<Playlist> getAllPlaylists(){
         return playlistRepo.findAll();
     }
 
-    @RequestMapping(value = "playlist", method = RequestMethod.POST)
+    @PostMapping(path = PLAYLIST_BASE_URL)
     public Playlist createPlaylist(@RequestBody Playlist playlist){
         return playlistRepo.saveAndFlush(playlist);
     }
--- src/main/java/com/geeknight/GeekNightApp/config/PlaylistApis.java
+++ src/main/java/com/geeknight/GeekNightApp/config/PlaylistApis.java
@@ -1,5 +1,16 @@
 package com.geeknight.GeekNightApp.config;
 
 public class PlaylistApis {
-    private static final String PLAYLIST_BASE_URL = "api/v1/playlist";
+    public static final String PLAYLIST_BASE_URL = "api/v1/playlist";
+    public static final String GET_PLAYLIST_BY_TITLE = PLAYLIST_BASE_URL + "/{title}";
+
+
+    //    Get all playlists	GET /playlist
+//    Get one playlist	GET /playlist/:playlistId
+//    Create a new playlist	POST /playlist
+//    Update a playlist	PUT /playlist/:playlistId
+//    Delete a playlist	DELETE /playlist/:playlistId
+//    Get all songs in a playlist	GET /playlist/:playlistId/songs
+//    Add a song to a playlist	POST /playlist/:playlistId/:songId
+//    Remove a song from a playlist	DELETE /playlist/:playlistId/:songId
 }
