cff1a39a375199ac1e0d99fbc963a7ed88f333c2
Rachelle <rchung989@gmail.com>
2018-08-02 17:41:38 -0400

    added song service

--- src/main/java/com/geeknight/GeekNightApp/services/SongService.java
+++ src/main/java/com/geeknight/GeekNightApp/services/SongService.java
@@ -1,5 +1,14 @@
 package com.geeknight.GeekNightApp.services;
 
+import com.geeknight.GeekNightApp.repositories.SongRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SongService {
+    private SongRepo songRepo;
+
+    @Autowired
+    public SongService(SongRepo songRepo){this.songRepo = songRepo;}
 }
 
