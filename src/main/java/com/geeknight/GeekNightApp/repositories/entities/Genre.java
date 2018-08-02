4fd3bdb481adfa9ead1a1e97f515adaffcc011bf
Chad <Hallinanchad@gmail.com>
2018-08-02 17:20:59 -0400

    genre entity completed

--- src/main/java/com/geeknight/GeekNightApp/repositories/entities/Genre.java
+++ src/main/java/com/geeknight/GeekNightApp/repositories/entities/Genre.java
@@ -1,6 +1,15 @@
 package com.geeknight.GeekNightApp.repositories.entities;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Genre {
+    private int id;
+    private String name;
     
+    public Genre(){}
 
 }
